4757e5d54eb8ceaa125a3d26c81f4e3ff4d9c7dd
SpotHeroMatt <spotheromatt@SH-MBPD-010.local>
2018-05-23 15:21:28 -0500

    More tests

--- herospotsite/organizer/tests.py
+++ herospotsite/organizer/tests.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 
 from django.test import TestCase
+from django.utils.six import BytesIO
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 
 from organizer.models import Reservation
 from organizer.serializers import ReservSerde
@@ -18,4 +21,9 @@ class ReservationTest(TestCase):
         resvr_serde = ReservSerde(resvr)
         print('start time {}'.format(resvr_serde.data['start_time']))
         print('stop time {}'.format(resvr_serde.data['stop_time']))
+        json = JSONRenderer().render(resvr_serde.data)
+        print(json)
+        stream = BytesIO(json)
+        data = JSONParser().parse(stream)
+        print(data)
         self.assertTrue(False)
